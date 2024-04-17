38e3e472477ff73ef91b58be276e0f56e29b66a7
苏阳 <2506806016@qq.com>
2024-04-17 19:56:52 +0800

    fix: 尝试修复BPU产生NPC不正确的问题

--- pipeline/BPU/BranchPredictionUnit.sv
+++ pipeline/BPU/BranchPredictionUnit.sv
@@ -34,36 +34,33 @@ module BranchPredictionUnit (
   output BpuRspSt bpu_rsp
 );
 
-  `RESET_LOGIC(clk,  a_rst_n, rst_n);
+  `RESET_LOGIC(clk, a_rst_n, rst_n);
   localparam NPC_OFS = $clog2(`FETCH_WIDTH) + 2;
 
 `ifdef NPC
   logic [31:0] pc, npc;
 
+  assign npc = bpu_req.redirect ? bpu_req.target : 
+               bpu_req.next ? {pc[31:NPC_OFS] + 1, {NPC_OFS{1'b0}}} : 
+               pc;
+
+
   always_comb begin
-    if (bpu_req.redirect) begin
-      npc = bpu_req.target;
-    end else if (bpu_req.next) begin
-      npc = {pc[31:NPC_OFS] + 1, {NPC_OFS{1'b0}}};
-    end begin
-      npc = pc;
-    end
 
     bpu_rsp.pc = pc;
+    bpu_rsp.npc = npc;
     bpu_rsp.valid = '1 & {`FETCH_WIDTH{bpu_req.next}};
     for (int i = 0; i < `FETCH_WIDTH; i++) begin
       if (i < pc[NPC_OFS - 1:2]) begin
         bpu_rsp.valid[i] = '0;
       end
     end
-    bpu_rsp.npc = npc;
   end
 
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
       pc <= 32'h1c00_0000;
-    end
-    else begin
+    end else begin
       pc <= npc;
     end
   end
