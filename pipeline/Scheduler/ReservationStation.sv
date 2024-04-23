c5df42bfffc91514c642cb3164a64f89cf40cd71
苏阳 <2506806016@qq.com>
2024-04-23 18:53:17 +0800

    fix(RS): 发射逻辑error

--- pipeline/Scheduler/ReservationStation.sv
+++ pipeline/Scheduler/ReservationStation.sv
@@ -132,7 +132,14 @@ parameter
     // select logic
     for (int i = 0; i < BANK_NUM; i++) begin
       // 选择每个bank最旧的指令
-      issue_valid_o[i] = '0;  // 初始化 ！！！
+      // 初始化 ！！！
+      issue_valid_o[i] = '0;
+      issue_base_o[i] = '0;
+      issue_oc_o[i] = '0;
+      issue_idx[i] = '0;
+      position_bit[i] = '0;
+      rob_idx[i] = '0;
+
       // 乱序发射
       for (int j = 0; j < BANK_SIZE; j++) begin
         // 是一条可发射的指令
