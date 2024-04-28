0e0f42a04f6dee7320dccd9769c9941b80d44d15
苏阳 <2506806016@qq.com>
2024-04-28 18:16:38 +0800

    fix(UNOPTFLAT): 尝试解决Circular combinational logic

--- pipeline/Scheduler/ReservationStation.sv
+++ pipeline/Scheduler/ReservationStation.sv
@@ -61,7 +61,7 @@ parameter
     OPTION_CODE oc;
   } RsEntrySt;
   localparam BANK_SIZE = RS_SIZE / BANK_NUM;
-  RsEntrySt [BANK_NUM - 1:0][BANK_SIZE - 1:0] rs_mem, rs_mem_n;
+  RsEntrySt [BANK_NUM - 1:0][BANK_SIZE - 1:0] rs_q, rs_n;
 
 /*=================================== Logic ===================================*/
   logic [BANK_NUM - 1:0][BANK_SIZE - 1:0] free;
@@ -72,17 +72,68 @@ parameter
   logic [BANK_NUM - 1:0][$clog2(`ROB_DEPTH) - 1:0] rob_idx;
   logic [BANK_NUM - 1:0][$clog2(BANK_SIZE) - 1:0] issue_idx;
 
-  always_comb begin
-    // defult assign
-    rs_mem_n = rs_mem;
+  // 生成free信号
+  always_comb begin : proc_free
     free = '0;
-    write_idx = '0;
-    position_bit = '0;
-    rob_idx = '0;
-    issue_idx = '0;
+    for (int i = 0; i < BANK_NUM; i++) begin
+      for (int j = 0; j < BANK_SIZE; j++) begin
+        free[i][j] = ~rs_q[i][j].base.valid | rs_q[i][j].base.issued;
+      end
+    end
+  end
+
+  // 生成wr_ready信号
+  for (genvar i = 0; i < BANK_NUM; i++) begin : gen_wr_ready_o
+    assign wr_ready_o[i] = $countones(free[i]) > 0;
+  end
+
+
+  // select
+  always_comb begin : proc_selec
+    // defult assign
     issue_valid_o = '0;
-    issue_base_o = '0;
-    issue_oc_o = '0;
+    issue_base_o  = '0;
+    issue_oc_o    = '0;
+    // to find the oldest instruction
+    position_bit  = '0;
+    rob_idx       = '0;
+    issue_idx     = '0;
+
+    // 如果position bit 相同，rob_idx 更小 的更旧
+    // 如果position bit 不同，rob_idx 更大 的更旧
+
+    for (int i = 0; i < BANK_NUM; i++) begin
+      // 选择每个bank最旧的指令
+      // 乱序发射
+      for (int j = 0; j < BANK_SIZE; j++) begin
+        // 是一条可发射的指令
+        if (!free[i][j] &&
+            (rs_q[i][j].base.psrc0_ready || !rs_q[i][j].base.psrc0_valid) &&
+            (rs_q[i][j].base.psrc1_ready || !rs_q[i][j].base.psrc1_valid)) begin
+          // 是一条更旧的指令 或 暂无有效的issue
+          if (issue_valid_o[i] == '0 ||
+              (rs_q[i][j].base.position_bit == position_bit[i] &&
+               rs_q[i][j].base.rob_idx < rob_idx[i]) || 
+              (rs_q[i][j].base.position_bit != position_bit[i] &&
+               rs_q[i][j].base.rob_idx > rob_idx[i])) begin
+            issue_valid_o[i] = '1;
+            issue_base_o[i]  = rs2is(rs_q[i][j].base);
+            issue_oc_o[i]    = rs_q[i][j].oc;
+
+            // 更新最老的指令idx
+            position_bit[i]  = rs_q[i][j].base.position_bit;
+            rob_idx[i]       = rs_q[i][j].base.rob_idx;
+            issue_idx[i]     = j;
+          end
+        end
+      end
+    end
+  end
+
+  always_comb begin : proc_rs_update
+    // defult assign
+    rs_n = rs_q;
+    write_idx = '0;
 
     // wake up logic
     // 先进行唤醒，以防写入的内容被覆盖
@@ -90,29 +141,18 @@ parameter
       for (int j = 0; j < BANK_SIZE; j++) begin
         for (int k = 0; k < `WB_WIDTH; k++) begin
           if (wb_i[k]) begin
-            if (rs_mem[i][j].base.valid &&
-                rs_mem[i][j].base.psrc0 == wb_pdest_i[k]) begin
-              rs_mem_n[i][j].base.psrc0_ready = '1;
+            if (rs_q[i][j].base.valid &&
+                rs_q[i][j].base.psrc0 == wb_pdest_i[k]) begin
+              rs_n[i][j].base.psrc0_ready = '1;
             end
-            if (rs_mem[i][j].base.valid &&
-                rs_mem[i][j].base.psrc1 == wb_pdest_i[k]) begin
-              rs_mem_n[i][j].base.psrc1_ready = '1;
+            if (rs_q[i][j].base.valid &&
+                rs_q[i][j].base.psrc1 == wb_pdest_i[k]) begin
+              rs_n[i][j].base.psrc1_ready = '1;
             end
           end
         end
       end
     end
-
-    for (int i = 0; i < BANK_NUM; i++) begin
-      for (int j = 0; j < BANK_SIZE; j++) begin
-        free[i][j] = ~rs_mem[i][j].base.valid | rs_mem[i][j].base.issued;
-      end
-    end
-
-    for (int i = 0; i < BANK_NUM; i++) begin
-      wr_ready_o[i] = $countones(free[i]) > 0;
-    end
-    
 
     // write logic
     for (int i = 0; i < BANK_NUM; i++) begin
@@ -125,40 +165,15 @@ parameter
       end
       
       if (wr_ready_o[i] && wr_valid_i[i]) begin
-        rs_mem_n[i][write_idx[i]].base = rs_base_i[i];
-        rs_mem_n[i][write_idx[i]].oc = option_code_i[i];
+        rs_n[i][write_idx[i]].base = rs_base_i[i];
+        rs_n[i][write_idx[i]].oc = option_code_i[i];
       end
     end
 
-    // select logic
+
     for (int i = 0; i < BANK_NUM; i++) begin
-      // 选择每个bank最旧的指令
-      // 乱序发射
-      for (int j = 0; j < BANK_SIZE; j++) begin
-        // 是一条可发射的指令
-        if (!free[i][j] &&
-            (rs_mem[i][j].base.psrc0_ready || !rs_mem[i][j].base.psrc0_valid) &&
-            (rs_mem[i][j].base.psrc1_ready || !rs_mem[i][j].base.psrc1_valid)) begin
-          // 是一条更旧的指令 或 暂无有效的issue
-          if (issue_valid_o[i] == '0 ||
-              (rs_mem[i][j].base.position_bit == position_bit[i] &&
-               rs_mem[i][j].base.rob_idx < rob_idx[i]) || 
-              (rs_mem[i][j].base.position_bit != position_bit[i] &&
-               rs_mem[i][j].base.rob_idx > rob_idx[i])) begin
-            issue_valid_o[i] = '1;
-            issue_base_o[i] = rs2is(rs_mem[i][j].base);
-            issue_oc_o[i] = rs_mem[i][j].oc;
-
-            // 更新最老的指令idx
-            position_bit[i] = rs_mem[i][j].base.position_bit;
-            rob_idx[i] = rs_mem[i][j].base.rob_idx;
-            issue_idx[i] = j;
-          end
-        end
-      end
-
       if (issue_valid_o[i] && issue_ready_i[i]) begin
-        rs_mem_n[i][issue_idx[i]].base.issued = '1;
+        rs_n[i][issue_idx[i]].base.issued = '1;
       end
     end
 
@@ -166,9 +181,9 @@ parameter
 
   always_ff @(posedge clk or negedge rst_n) begin
     if(~rst_n || flush_i) begin
-      rs_mem <= '0;
+      rs_q <= '0;
     end else begin
-      rs_mem <= rs_mem_n;
+      rs_q <= rs_n;
     end
   end
 
